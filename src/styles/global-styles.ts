f2a8df36e9c577da1a1313fbdd98955fe7958efb
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-06-06 16:05:01 +0900

    Update global-styles.ts

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -2,17 +2,29 @@ import {createGlobalStyle} from 'styled-components';
 import {normalize} from 'styled-normalize';
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Gothic+A1:wght@300&display=swap');
   ${normalize}
-  html {
-    box-sizing: border-box;
-    font-size: 62.5%;
-    min-width: 320px;
-  }
   *,
   *::before,
   *::after {
     box-sizing: inherit;
   }
-  * { font-family: 'Spoqa Han Sans', 'Spoqa Han Sans JP', 'Sans-serif';}
   a { cursor: pointer; text-decoration: none; }
+  body{
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  * {
+    font-family: "Noto Sans KR", sans-serif;
+    background-color: initial;
+    margin: 0px;
+    padding: 0px;
+    border: none;
+    outline: none;
+    text-decoration: none;
+    font-size: 16px;
+    color: black;
+    font-weight: 400;
+  }
 `;
