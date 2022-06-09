261a9649bf3d2dcd4452ad9b2b1f3d8b053e078c
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-06-09 21:49:29 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ import Image from 'next/image'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  background-color:red;
+  width:100%;
+  min-height:100vh;
 `
 
 const Home: NextPage = () => {
   return (
     <Container>
-      hellooooo
+      NextJs Web
     </Container>
   )
 }
