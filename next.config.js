01bf519f80f9e711cdaa5251eac16fc0af41bfc6
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-06-21 22:47:17 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
-  }
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
