55f7108dac52e6e6bc6c20ff3547e6d206793d74
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-07-28 08:55:25 +0900

    Update ssr.tsx

--- src/pages/ssr.tsx
+++ src/pages/ssr.tsx
@@ -1,14 +1,8 @@
-// import { withSSRContext } from 'aws-amplify'
 import type { GetServerSideProps, NextPage } from 'next'
-import Head from 'next/head'
 import styled from 'styled-components'
-// import { getPost } from "../../graphql/queries"
-// import { deletePost as deletePostQuery } from "../../graphql/mutations"
-// import { IPost } from '../../types';
 
 const SsrPage: NextPage<{ data: { data: string } }> = ({ data }) => {
   const ssrData = data.data;
-
   return (
     <Container>
       {ssrData}
@@ -17,14 +11,6 @@ const SsrPage: NextPage<{ data: { data: string } }> = ({ data }) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  // const SSR = withSSRContext({ req });
-  // const { data } = await SSR.API.graphql({
-  //   query: getPost,
-  //   variables: {
-  //     id: params?.id
-  //   }
-  // });
-
   const originUrl = req.headers.host || "http://localhost:3003";
   const fetchUrl = `${originUrl.search("http://") !== -1 ? originUrl : ("http://" + originUrl)}/api/hello`;
   const fetchResult = await fetch(fetchUrl).then(response => response.json());
