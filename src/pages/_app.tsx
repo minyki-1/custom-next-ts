1eeb12f1df4fc3fb5f988532dbafa7698054261d
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-07-30 20:59:17 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,5 @@
-import '../../styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-// import { Amplify } from 'aws-amplify';
-// import awsExports from '../aws-exports';
-// Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
