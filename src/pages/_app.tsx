f88affeeeb1d0e11d4a3e54880e30d4492303114
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-10-28 20:52:13 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
-import '../styles/reset.css'
-import '../styles/globals.css'
+import '../globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
