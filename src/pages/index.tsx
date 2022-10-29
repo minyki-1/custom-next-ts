e61eec956af41aea9a4943d503d55c0d91f9275e
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-10-30 01:14:12 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import SvgHeart from '../svg/heart.svg';
 
 export default function Home() {
   return (
     <Container>
       <h1>My Next WebApp</h1>
+      <SvgHeart width="24" height="24" fill="white" />
     </Container>
   )
 }
