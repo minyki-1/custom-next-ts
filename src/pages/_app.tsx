8865d3c7d88f06679fac93c4648934bb745c359c
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-11-10 20:35:03 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+
+const font = Inter();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html { font-family: ${font.style.fontFamily}; }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
