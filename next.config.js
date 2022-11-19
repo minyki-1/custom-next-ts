c001577b1e20b485147dcb5813bc93552deee8d5
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-11-20 03:24:19 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   // experimental: { appDir: true }, // NextJs 13버전 기능을 사용할려면 추가
-  reactStrictMode: true,
+  reactStrictMode: true, // true면 문제 감지와 경고를 위해 두번 렌더링함
   swcMinify: true,
   compiler: {
     styledComponents: true,
