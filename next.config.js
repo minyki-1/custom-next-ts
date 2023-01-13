69b7816aa7c2018c624a4b81b98eea01d15af6c2
CWIN <cwstbp0426@gmail.com>
2023-01-13 22:23:24 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
