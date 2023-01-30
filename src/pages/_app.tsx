9993d7ac05ecb3903e0f670703d9a61614b917f3
CWIN <cwstbp0426@gmail.com>
2023-01-30 16:24:44 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from '@next/font/google'
 
-const font = Inter();
+const font = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
